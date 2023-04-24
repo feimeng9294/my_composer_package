0b37352aff8c6310490af400d9542dc2639199e4
侯哥 <1019887718@qq.com>
2023-04-24 10:49:55 +0800

    Add files via upload

--- src/library/StrUtil.php
+++ src/library/StrUtil.php
@@ -1,1 +1,19 @@
-dddddd
+<?php
+
+
+namespace Monkey\MyComposerPackage\library;
+
+/**
+* Created by IntelliJ IDEA.
+*
+* @Author : monkey
+* @create 2023/4/8 23:42
+*/
+class StrUtil
+{
+
+    public function strLen($str){
+        return strlen($str);
+    }
+
+}
